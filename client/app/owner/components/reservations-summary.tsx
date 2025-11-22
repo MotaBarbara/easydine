68bb871c1a43f69f9fee7930c87ce5800fcd6b94
MotaBarbara <barbarantmotaa@gmail.com>
2025-11-22 10:47:16 +0100

    Fix import path in reservations-summary

--- client/app/owner/components/reservations-summary.tsx
+++ client/app/owner/components/reservations-summary.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import type { Reservation } from "../../hooks/useDashboardData";
+import type { Reservation } from "../hooks/useDashboardData";
 
 type Props = {
   reservations: Reservation[];
